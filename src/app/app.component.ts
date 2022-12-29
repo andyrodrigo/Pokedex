9d492614a503a72a2a6cb462b6ecbe91b29cbdc4
Anderson Benicio <andyrodrigo2@gmail.com>
2022-12-29 08:26:49 -0300

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'Pokedex';
+
+  somar(n1: string, n2: string) {
+    const resposta = Number(n1) + Number(n2);
+    alert(resposta);
+  }
 }
